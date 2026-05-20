958e8f1cb6e52b31edc529f01dea126508fc48a2
kael <theo.viti2@outlook.fr>
2026-05-20 09:20:30 +0200

    feat: merging to webapp-security

--- patients/src/main/java/com/example/patients/controller/UserController.java
+++ patients/src/main/java/com/example/patients/controller/UserController.java
@@ -35,6 +35,11 @@ public class UserController {
                 return userService.findByUserName(username);
         }
 
+        @GetMapping("/by-username/{username}")
+        public User findByUserName(@PathVariable String username) {
+                return userService.findByUserName(username);
+        }
+
         @PostMapping
         public User addUser(@RequestBody User user) {
                 return userService.addUser(user);
