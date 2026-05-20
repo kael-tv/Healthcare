4ccce0e5f5cd3db2dec9f986e19e6ce9ef3ca7e1
kael <theo.viti2@outlook.fr>
2026-05-20 09:58:25 +0200

    fix: normalize package name casing (Exception -> exception)

--- patients/src/main/java/com/example/patients/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.patients.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-        public ResourceNotFoundException(String message) {
-                super(message);
-        }
-}
