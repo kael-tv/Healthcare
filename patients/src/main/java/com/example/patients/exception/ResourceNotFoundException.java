294f6b480bab37082b2a69ff24103b71e32a1004
kael <theo.viti2@outlook.fr>
2026-05-20 09:46:36 +0200

    refactor: standardize webapp package names to lowercase

--- patients/src/main/java/com/example/patients/exception/ResourceNotFoundException.java
+++ patients/src/main/java/com/example/patients/exception/ResourceNotFoundException.java
@@ -1,4 +1,4 @@
-package com.example.patients.Exception;
+package com.example.patients.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
         public ResourceNotFoundException(String message) {
